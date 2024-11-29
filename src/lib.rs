dae3827f9f36f7e83de76c744caf2b25ebfe1a07
Santiago Ontañón Sánchez <sontanon@gmail.com>
2024-11-29 15:40:39 -0600

    Add the SpiceAmount and SpiceAmountBuilder structs to avoid using arrays.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use thiserror::Error;
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 /// This represents a single spice cube.
 ///
@@ -76,6 +78,70 @@ impl SpiceCube {
     }
 }
 
+#[derive(Debug, Default, PartialEq, Copy, Clone)]
+pub struct SpiceAmount {
+    turmeric: u8,
+    saffron: u8,
+    cardamon: u8,
+    cinnamon: u8,
+    vector: [u8; 4],
+}
+
+pub struct SpiceAmountBuilder {
+    spice_amount: SpiceAmount,
+}
+
+impl SpiceAmountBuilder {
+    pub fn new() -> Self {
+        Self {
+            spice_amount: SpiceAmount::default(),
+        }
+    }
+
+    pub fn turmeric(mut self, turmeric: u8) -> Self {
+        self.spice_amount.turmeric = turmeric;
+        self.spice_amount.vector[0] = turmeric;
+        self
+    }
+
+    pub fn saffron(mut self, saffron: u8) -> Self {
+        self.spice_amount.saffron = saffron;
+        self.spice_amount.vector[1] = saffron;
+        self
+    }
+
+    pub fn cardamon(mut self, cardamon: u8) -> Self {
+        self.spice_amount.cardamon = cardamon;
+        self.spice_amount.vector[2] = cardamon;
+        self
+    }
+
+    pub fn cinnamon(mut self, cinnamon: u8) -> Self {
+        self.spice_amount.cinnamon = cinnamon;
+        self.spice_amount.vector[3] = cinnamon;
+        self
+    }
+
+    pub fn build(self) -> SpiceAmount {
+        self.spice_amount
+    }
+}
+
+impl From<[u8; 4]> for SpiceAmount {
+    fn from(spice_array: [u8; 4]) -> SpiceAmount {
+        SpiceAmountBuilder::new()
+            .turmeric(spice_array[0])
+            .saffron(spice_array[1])
+            .cardamon(spice_array[2])
+            .cinnamon(spice_array[3])
+            .build()
+    }
+}
+impl Into<[u8; 4]> for SpiceAmount {
+    fn into(self) -> [u8; 4] {
+        self.vector
+    }
+}
 /// Maximum number of spice cubes a caravan can hold.
 pub const MAX_CARAVAN_SIZE: usize = 10;
 
@@ -92,17 +158,16 @@ impl Caravan {
     pub fn get_spaces(&self) -> &[Option<SpiceCube>; MAX_CARAVAN_SIZE] {
         &self.spaces
     }
-    /// Creates a new `Caravan` from an array of spice amounts.
-    ///
-    /// `spice_array` represents the quantities `[turmeric, saffron, cardamon, cinnamon]` (notice the ordering).
+    /// Creates a new `Caravan` from a spice amount.
     ///
     /// # Examples
     ///
     /// Create a caravan with 3 turmeric cubes (e.g., for the 1st player):
     ///
     /// ```
-    /// use libcsr::{SpiceCube::Turmeric, Caravan};
-    /// let caravan_1st_player = Caravan::from_spice_array([3, 0, 0, 0]).unwrap();
+    /// use libcsr::{SpiceCube::Turmeric, SpiceAmount, SpiceAmountBuilder, Caravan};
+    /// let spice_amount = SpiceAmountBuilder::new().turmeric(3).build();
+    /// let caravan_1st_player = Caravan::from_spice_amount(spice_amount).unwrap();
     /// assert_eq!(caravan_1st_player.current_capacity(), 3);
     /// assert_eq!(caravan_1st_player.get_spaces(), &[Some(Turmeric), Some(Turmeric), Some(Turmeric), None, None, None, None, None, None, None]);
     /// ```
@@ -110,8 +175,9 @@ impl Caravan {
     /// Create a caravan with 3 turmeric cubes, and 1 saffron cube (e.g., for the 5th player):
     ///
     /// ```
-    /// use libcsr::{SpiceCube::{Turmeric, Saffron}, Caravan};
-    /// let caravan_5th_player = Caravan::from_spice_array([3, 1, 0, 0]).unwrap();
+    /// use libcsr::{SpiceCube::{Turmeric, Saffron}, SpiceAmount, SpiceAmountBuilder, Caravan};
+    /// let spice_amount = SpiceAmountBuilder::new().turmeric(3).saffron(1).build();
+    /// let caravan_5th_player = Caravan::from_spice_amount(spice_amount).unwrap();
     /// assert_eq!(caravan_5th_player.current_capacity(), 4);
     /// assert_eq!(caravan_5th_player.get_spaces(), &[Some(Turmeric), Some(Turmeric), Some(Turmeric), Some(Saffron), None, None, None, None, None, None]);
     /// ```
@@ -119,12 +185,15 @@ impl Caravan {
     ///
     /// This function will return an error if the total number of spice cubes exceeds [`MAX_CARAVAN_SIZE`].
     /// ```
-    /// use libcsr::{Caravan, GameErrors, MAX_CARAVAN_SIZE};
-    /// let result = Caravan::from_spice_array([MAX_CARAVAN_SIZE as u8, 1, 0, 0]);
+    /// use libcsr::{Caravan, GameErrors, MAX_CARAVAN_SIZE, SpiceAmount, SpiceAmountBuilder};
+    /// let spice_amount = SpiceAmountBuilder::new().turmeric(MAX_CARAVAN_SIZE as u8).cinnamon(1).build();
+    /// let result = Caravan::from_spice_amount(spice_amount);
     /// assert_eq!(result, Err(GameErrors::MaxSpiceCapacityReached));
     /// ```
-    pub fn from_spice_array(spice_array: [u8; 4]) -> Result<Self, GameErrors> {
-        let [turmeric, saffron, cardamon, cinnamon] = spice_array.map(|x| x as usize);
+    pub fn from_spice_amount(spice_amount: SpiceAmount) -> Result<Self, GameErrors> {
+        // This copies, it does not move-out.
+        let spice_vector: [u8; 4] = spice_amount.into();
+        let [turmeric, saffron, cardamon, cinnamon] = spice_vector.map(|x| x as usize);
         let total = turmeric + saffron + cardamon + cinnamon;
 
         if total > MAX_CARAVAN_SIZE {
@@ -157,38 +226,45 @@ impl Caravan {
         self.spaces.iter().flatten().count() as u8
     }
 
-    /// Get an array with the count of the current spices in the caravan.
-    /// This follows the convention `[turmeric, saffron, cardamon, cinnamon]`.
+    /// Get the `SpiceAmount` in the caravan.
     ///
     /// # Examples
     /// ```
-    /// use libcsr::Caravan;  
-    /// let caravan = Caravan::from_spice_array([3, 1, 2, 4]).unwrap();
-    /// assert_eq!(caravan.current_spices(), [3, 1, 2, 4]);
+    /// use libcsr::{Caravan, SpiceAmountBuilder, SpiceAmount};  
+    /// let spice_amount = SpiceAmountBuilder::new().turmeric(3).saffron(1).cardamon(2).cinnamon(4).build();
+    /// let caravan = Caravan::from_spice_amount(spice_amount).unwrap();
+    /// let expected_spice_amount = SpiceAmount::from([3, 1, 2, 4]);
+    /// assert_eq!(caravan.get_spice_amount(), expected_spice_amount);
     /// ```
-    pub fn current_spices(&self) -> [u8; 4] {
-        [
-            self.spaces
-                .iter()
-                .filter(|&&x| x == Some(SpiceCube::Turmeric))
-                .count() as u8,
-            self.spaces
-                .iter()
-                .filter(|&&x| x == Some(SpiceCube::Saffron))
-                .count() as u8,
-            self.spaces
-                .iter()
-                .filter(|&&x| x == Some(SpiceCube::Cardamon))
-                .count() as u8,
-            self.spaces
-                .iter()
-                .filter(|&&x| x == Some(SpiceCube::Cinnamon))
-                .count() as u8,
-        ]
+    pub fn get_spice_amount(&self) -> SpiceAmount {
+        SpiceAmountBuilder::new()
+            .turmeric(
+                self.spaces
+                    .iter()
+                    .filter(|&&x| x == Some(SpiceCube::Turmeric))
+                    .count() as u8,
+            )
+            .saffron(
+                self.spaces
+                    .iter()
+                    .filter(|&&x| x == Some(SpiceCube::Saffron))
+                    .count() as u8,
+            )
+            .cardamon(
+                self.spaces
+                    .iter()
+                    .filter(|&&x| x == Some(SpiceCube::Cardamon))
+                    .count() as u8,
+            )
+            .cinnamon(
+                self.spaces
+                    .iter()
+                    .filter(|&&x| x == Some(SpiceCube::Cinnamon))
+                    .count() as u8,
+            )
+            .build()
     }
 }
-
-use thiserror::Error;
 
 #[derive(Debug, Error, PartialEq)]
 pub enum GameErrors {
@@ -203,6 +279,24 @@ pub enum GameErrors {
 
     #[error("Internal logic error occurred")]
     InternalLogicError,
+}
+
+enum PlayerAction {
+    PlayCard,
+    AcquireCard,
+    Rest,
+    Score,
+}
+
+enum ActionCard {
+    SpiceCard([u8; 4]),
+    ExchangeCard,
+    UpgradeCard(u8),
+}
+
+struct PointsCard {
+    points: u8,
+    spices: [u8; 4],
 }
 
 #[cfg(test)]
